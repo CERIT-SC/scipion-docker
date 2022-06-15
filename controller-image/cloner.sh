c393d839aa066a18d92a2eab9b658e17a1595068
Josef Handl <handljosef@gmail.com>
2022-06-15 14:09:26 +0000

    new cloner as part of the controller instead of k8s pod

---
+++ controller-image/cloner.sh
@@ -0,0 +1,134 @@
+#!/bin/sh
+
+set -ex
+
+dir_od_p="/mnt/od-project"
+dir_vol_p="/mnt/vol-project"
+dir_cloner="/opt/cloner"
+dir_tmp="${dir_cloner}/tmp"
+
+dir_od_s="/mnt/od-source"
+dir_vol_s="/mnt/vol-source"
+
+dir_scipion="scipion-docker"
+file_symlink_dump="symlink-dump"
+file_project_lock="project.lock"
+file_instance_status="instance-status"
+file_instance_log="instance.log"
+
+file_instance_log_path="${dir_vol_p}/${dir_scipion}/${file_instance_log}"
+
+dir_od_p_path="${dir_od_p}/$(ls ${dir_od_p})"
+dir_vol_p_path="${dir_vol_p}"
+
+dir_od_s_path="${dir_od_s}/$(ls ${dir_od_s})"
+dir_vol_s_path="${dir_vol_s}"
+
+rsync_options="--delete --recursive --times --omit-dir-times --info=progress2"
+
+echo "${dir_od_p_path}/"
+echo "${dir_od_s_path}/"
+
+# vol-project > od-project
+#--------------------------
+save_project () {
+	mkdir -p "${dir_tmp}"
+
+	# rsync vol-project > tmp
+	rsync $rsync_options --links "${dir_vol_p_path}/" "${dir_tmp}/"
+
+	# create symlinks dump
+	rm "${dir_tmp}/${dir_scipion}/${file_symlink_dump}" || true
+	cd "${dir_tmp}/"
+	for link in $(find -L "./" -xtype l); do
+		link_target="$(readlink ${link})"
+		echo "${link_target} ${link}" >> "${dir_tmp}/${file_symlink_dump}"
+	done
+
+	# remove symlinks
+	cd "${dir_tmp}/"
+	for link in $(find -L "./" -xtype l); do
+		rm "${dir_tmp}/${link}"
+	done
+
+	# rsync tmp > od-project
+	rsync $rsync_options "${dir_tmp}/" "${dir_od_p_path}/"
+
+	# remove project lock in OD
+	if [ "$1" = "unlock" ]; then
+		rm "${dir_od_p_path}/${dir_scipion}/${file_project_lock}"
+		echo "The project lock has been removed" >> "$file_instance_log_path"
+	fi
+}
+autosave_project () {
+	echo "Autosaving the project" >> "$file_instance_log_path"
+	save_project
+	echo "Autosave has been completed" >> "$file_instance_log_path"
+}
+trapsave_project () {
+	echo "Saving the project due to instance termination" >> "$file_instance_log_path"
+	save_project unlock
+	echo "Save has been completed" >> "$file_instance_log_path"
+}
+
+# od-project > vol-project
+#--------------------------
+restore_project () {
+	echo "Restoring the project" >> "$file_instance_log_path"
+
+	# check project lock
+	if [ -f "${dir_od_p_path}/${dir_scipion}/${file_project_lock}" ]; then
+		echo "The project is open in another instance" >> "$file_instance_log_path"
+		exit 1
+	fi
+
+	# remove last instance files
+	mkdir -p "${dir_tmp}"
+	mkdir -p "${dir_od_p_path}/${dir_scipion}/"
+	rm "${dir_od_p_path}/${dir_scipion}/${file_instance_status}" || true
+	rm "${dir_od_p_path}/${dir_scipion}/${file_instance_log}" || true
+
+	# lock the project in OD
+	touch "${dir_od_p_path}/${dir_scipion}/${file_project_lock}"
+	echo "The project has been locked so that it cannot be opened from another instance of Scipion" >> "$file_instance_log_path"	
+
+	# rsync od-project > tmp
+	rsync $rsync_options --exclude "${dir_scipion}/" "${dir_od_p_path}/" "${dir_tmp}/" > "${dir_cloner}/progress-restore.log"
+	#rsync $rsync_options --exclude "${dir_tmp}/${dir_scipion}/${file_instance_log}" "${dir_od_p_path}/" "${dir_tmp}/"
+
+	# restore symlinks in tmp dir
+	cd "${dir_tmp}/"
+	while read line; do
+		link_target=$(echo $line | awk '{ print $1 }')
+		link=$(echo $line | awk '{ print $2 }')
+		ln -s "$link_target" "$link"
+	done < "${dir_tmp}/${dir_scipion}/${file_symlink_dump}"
+
+	# rsync tmp > vol-project
+	rsync $rsync_options --links --exclude "${dir_scipion}/" "${dir_tmp}/" "${dir_vol_p_path}/" > "${dir_cloner}/progress-restore.log"
+	#rsync $rsync_options --links --exclude "${dir_vol_p_path}/${dir_scipion}/${file_instance_log}" "${dir_tmp}/" "${dir_vol_p_path}/"
+
+	echo "Restore has been completed" >> "$file_instance_log_path"
+}
+
+# od-source > vol-source
+#--------------------------
+clone_source () {
+	echo "Cloning a source data" >> "$file_instance_log_path"
+	rsync -av --delete --no-perms --omit-dir-times "${dir_od_s_path}/" "${dir_vol_s_path}/" > "${dir_cloner}/progress-clone.log"
+	echo "Cloning has been completed" >> "$file_instance_log_path"
+}
+
+if [ "$1" = "autosave" ]; then
+	autosave_project
+elif [ "$1" = "trapsave" ]; then
+	trapsave_project
+elif [ "$1" = "restore" ]; then
+	restore_project
+elif [ "$1" = "clone" ]; then
+	clone_source
+else
+	echo "No action has been specified"
+	exit 1
+fi
+
--- controller-image/docker-entrypoint.sh
+++ controller-image/docker-entrypoint.sh
@@ -1,84 +1,59 @@
 #!/bin/bash
 
-check_status () {
-	local pod="$1"
+set -ex
 
-	pod_k8s_line=$(kubectl get pod | grep "$pod")
-	pod_fullname=$(echo "$pod_k8s_line" | awk '{print $1}')
-	pod_status=$(echo "$pod_k8s_line" | awk '{print $3}')
-
-	if [ -z "$pod_status" ]; then
-		echo "${pod}: Container was unexpectedly terminated." >> "$file_log"
-	elif [ "$pod_status" = "Completed" ]; then
-		echo "${pod}: Container completed successfully." >> "$file_log"
-
-		return 0
-
-	elif [ "$pod_status" = "ImagePullBackOff" ] ||
-	     [ "$pod_status" = "ErrImagePull" ]; then
-		echo "" >> "$file_log"
-		echo "${pod}: Container cannot be started. K8S description:" >> "$file_log"
-		sleep 2
-		kubectl describe pod "$pod_fullname" >> "$file_log"
-		echo ""
-		echo "${pod}: Instance startup failed. Check the log in the \"controller\" container: \"${file_log}\"" >> "$file_log"
-
-		return 0
-
-	elif [ "$pod_status" = "CrashLoopBackOff" ] ||
-	     [ "$pod_status" = "Error" ]; then
-		echo "" >> "$file_log"
-		echo "${pod}: Container failed. K8S log:" >> "$file_log"
-		sleep 2
-
-		kubectl logs "$pod_fullname" >> "$file_log"
-		echo ""
-		echo "${pod}: Instance startup failed. Check the log in the \"controller\" container: \"${file_log}\"" >> "$file_log"
-
-		return 0
-
-	elif [ "$pod_status" = "ContainerCreating" ]; then
-		echo "${pod}: Container is still creating." >> "$file_log"
-	elif [ "$pod_status" = "Pending" ]; then
-		echo "${pod}: Pending launch." >> "$file_log"
-	elif [ "$pod_status" = "Running" ]; then
-		echo "${pod}: Container is still running." >> "$file_log"
-	fi
-
-	return 1
-}
-
-#auto_save () {
-#}
-
-trap_save () {
-	echo "This Scipion instance has received a stop signal. The Scipion application will be terminated and the project saved to the Onedata." >> "$file_log"
-
-	kubectl delete --force job scipion-docker-backup || true
-	export SUBST_CLONER_ACTION="backup"
-	export SUBST_CLONER_REMOVE_LOCK="true"
-	pod_saver=$(envsubst < /opt/deploy_cloner.yaml | kubectl apply -f - | awk '{print $1}' | cut -d'/' -f2)
-
-	while ! check_status "scipion-docker-backup"; do
-		sleep 10
-	done
-	
-	echo "Project is saved. This instance is being deleted." >> "$file_log"
-
-	kubectl delete \
-		job.batch/scipion-cloner-backup \
-		job.batch/scipion-cloner-restore \
-		job.batch/scipion-cloner-clone \
-		deployment.apps/scipion-master || true
-}
-
-set -x
+dir_od="/mnt/od-source"
+dir_vol="/mnt/vol-source"
 
 dir_scipion="/mnt/vol-project/scipion-docker"
 file_log="${dir_scipion}/instance.log"
 file_status="${dir_scipion}/instance-status"
 
-export SUBST_NAMESPACE=handl-ns
+dir_cloner="/opt/cloner"
+
+# Check the mounts
+#====================
+
+exit_f () {
+        echo "The instance cannot be started"
+        exit 1
+}
+
+test_dir () {
+        if ! test -d "$1"; then
+                echo "The mountpoint \"${1}\" missing"
+                exit_f
+        fi
+}
+
+test_dir "$dir_od"
+test_dir "$dir_vol"
+
+if [ $(ls "${dir_od}/" | wc -l) != "1" ]; then
+        echo "There are more or less Onedata spaces than one in the \"${dir_od}\" path."
+        exit_f
+fi
+
+dir_od_path="${dir_od}/$(ls "${dir_od}/")"
+
+if [ $(ls "$dir_od_path" | wc -l) = "0" ]; then
+        echo "There is no data in the Onedata mount \"${dir_od_path}\""
+        exit_f
+fi
+
+trapsave () {
+	echo "This Scipion instance has received a stop signal. The Scipion application will be terminated and the project saved to the Onedata" >> "$file_log"
+
+	if [ kill -SIGTERM $pid_autosave ]; then
+		"The autosave was terminated" >> "$file_log"
+	fi
+
+	./cloner.sh trapsave
+
+	echo "Project is saved. Instance is being deleted" >> "$file_log"
+
+	exit 0
+}
 
 mkdir -p "${dir_scipion}"
 rm "${file_status}" || true
@@ -86,67 +61,69 @@ rm "${file_log}" || true
 
 echo "Deployment created" >> "$file_log"
 
-# Run cloner
-export SUBST_CLONER_ACTION="clone"
-echo "Copying raw source data from Onedata storage" >> "$file_log"
-pod_cloner=$(envsubst < /opt/deploy_cloner.yaml | kubectl apply -f - | awk '{print $1}' | cut -d'/' -f2)
+# Start cloner-clone and cloner-restore
+#==========================================
 
-echo "debug - pod clone ${pod_cloner}"
+./cloner.sh clone &
+pid_clone=$!
 
-# Run syncer restore
-export SUBST_CLONER_ACTION="restore"
-echo "Restoring project data from Onedata storage" >> "$file_log"
-pod_syncer=$(envsubst < /opt/deploy_cloner.yaml | kubectl apply -f - | awk '{print $1}' | cut -d'/' -f2)
+./cloner.sh restore &
+pid_restore=$!
 
-echo "debug - pod restore ${pod_syncer}"
+update_progress () {
+	# remove the previous line from the log
+	sed -i "/${1}:.*$/d" "$file_log"
 
-pod_list=("$pod_cloner" "$pod_syncer")
-pod_list_stopped=("false" "false")
-
-count_completed=0
-while [ "${pod_list_stopped[0]}" = "false" ] || [ "${pod_list_stopped[1]}" = "false" ]; do
-	for pod_index in 0 1; do
-		if [ "${pod_list_stopped[$pod_index]}" = "true" ]; then
-			continue 1
-		fi
-
-		pod="${pod_list[$pod_index]}"
-
-		if check_status "$pod"; then
-			pod_list_stopped[$pod_index]="true"
-		fi
-
-		sleep 30
-	done
-done
-
-echo "je mozne spustit scipion"
-echo "ok" >> "$file_status"
-
-#kubectl delete job.batch/scipion-cloner-clone job.batch/scipion-cloner-restore || true
-
-trap trap_save EXIT 
+	# print new line with rsync progress
+	progress=$(sed 's/.*\r//' "$2" | awk '{print $2}')
+	echo "${1}: ${progress}" >> "$file_log"
+}
 
 while true; do
-	sleep 600
+	sleep 5
 
-	previous_cloner=$(kubectl get pod | grep scipion-cloner-backup | awk '{print $3}')
-	if [ "Running" = "${previous_cloner}" ]; then
-		echo "The previous autosave is still running. The new one will not start now." >> "$file_log"
-		return 1
+	if ps -p $pid_clone > /dev/null; then
+		update_progress "Cloning" "${dir_cloner}/progress-clone.log"
+	elif ! wait $pid_clone; then
+		echo "Cloning failed" >> "$file_log"
+		pid_clone=0
+		exit_f
 	fi
-	
-	echo "Autosaving the project" >> "$file_log"
 
-	kubectl delete --force job scipion-docker-backup || true
-	
-	export SUBST_CLONER_ACTION=backup
-	pod_saver=$(envsubst < /opt/deploy_cloner.yaml | kubectl apply -f - | awk '{print $1}' | cut -d'/' -f2)
+	if ps -p $pid_restore > /dev/null; then
+		update_progress "Restoring" "${dir_cloner}/progress-restore.log"
+	elif ! wait $pid_restore; then
+		echo "Restore failed" >> "$file_log"
+		pid_restore=0
+		exit_f
+	fi
+
+	if ! ps -p $pid_clone > /dev/null && ! ps -p $pid_restore > /dev/null; then
+		break 1
+	fi
 done
 
+# Send "signal" to start desktop environment
+# and start autosaving
+#===============================================
+
+echo "ok" >> "$file_status"
+
+trap trapsave EXIT
+
+set +e
+
+while true; do
+	sleep 30
+	./cloner.sh autosave &
+	pid_autosave=$!
+	if ! wait $pid_autosave; then
+		echo "Autosave failed" >> "$file_log"
+	fi
+done
 
 # TODO kill and wait (pozor na timeout v k8s podu)
-echo "Saving the project" >> "$file_log"
+#echo "Saving the project" >> "$file_log"
 # 
 # TODO provest "ls" pro force updatu adresaru v k8s volumu! | kvuli logum | Ales
  
