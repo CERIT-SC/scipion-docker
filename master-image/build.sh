aae97c515da5868872ab191b014d0e6a55779dc4
Josef Handl <handljosef@gmail.com>
2023-01-16 12:42:07 +0000

    master, tool: fix build.sh scripts

--- master-image/build.sh
+++ master-image/build.sh
@@ -6,10 +6,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-if [ "$1" != "--nobase" ]; then
-        ../base-image/build.sh
-fi
-
 docker build -t jhandl/scipion-master:$tag .
 docker build -t hub.cerit.io/josef_handl/scipion-master:$tag .
 
--- tool-image/build.sh
+++ tool-image/build.sh
@@ -16,26 +16,20 @@ docker-build-push () {
 }
 
 build-tool () {
-	docker-build-push "appion"     --build-arg SD_PLUGIN="scipion-em-appion"                                 &
-	docker-build-push "cistem"     --build-arg SD_PLUGIN="scipion-em-cistem"                                 &
-	docker-build-push "eman2"      --build-arg SD_PLUGIN="scipion-em-eman2"                                  &
-	docker-build-push "gautomatch" --build-arg SD_PLUGIN="scipion-em-gautomatch"                             &
-	docker-build-push "gctf"       --build-arg SD_PLUGIN="scipion-em-gctf" --build-arg SD_BIN="gctf-1.18"    &
-	docker-build-push "motioncorr" --build-arg SD_PLUGIN="scipion-em-motioncorr"                             &
-	docker-build-push "phenix"     --build-arg SD_PLUGIN="scipion-em-phenix"                                 &
-	docker-build-push "pwem"                                                                                 &
-	docker-build-push "relion"     --build-arg SD_PLUGIN="scipion-em-relion" --build-arg SD_BIN="relion-4.0" &
-	docker-build-push "spider"     --build-arg SD_PLUGIN="scipion-em-spider"                                 &
-	docker-build-push "xmipp3"                                                                               &
+	docker-build-push "appion"     --build-arg SD_PLUGIN="scipion-em-appion"                                 #&
+	docker-build-push "cistem"     --build-arg SD_PLUGIN="scipion-em-cistem"                                 #&
+	docker-build-push "eman2"      --build-arg SD_PLUGIN="scipion-em-eman2"                                  #&
+	docker-build-push "gautomatch" --build-arg SD_PLUGIN="scipion-em-gautomatch"                             #&
+	docker-build-push "gctf"       --build-arg SD_PLUGIN="scipion-em-gctf" --build-arg SD_BIN="gctf-1.18"    #&
+	docker-build-push "motioncorr" --build-arg SD_PLUGIN="scipion-em-motioncorr"                             #&
+	docker-build-push "phenix"     --build-arg SD_PLUGIN="scipion-em-phenix"                                 #&
+	docker-build-push "pwem"                                                                                 #&
+	docker-build-push "relion"     --build-arg SD_PLUGIN="scipion-em-relion" --build-arg SD_BIN="relion-4.0" #&
+	docker-build-push "spider"     --build-arg SD_PLUGIN="scipion-em-spider"                                 #&
+	docker-build-push "xmipp3"                                                                               #&
 }
 
-set -e
-
 cd "$(dirname "$0")"
-
-if [ "$1" != "--nobase" ]; then
-	../base-image/build.sh
-fi
 
 build-tool
 
