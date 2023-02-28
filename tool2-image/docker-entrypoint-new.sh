4d6afcd1799538f0e2b0c006d72e90a26981fde5
Josef Handl <handljosef@gmail.com>
2023-02-28 08:20:44 +0000

    tool2, master: working prototype

--- tool2-image/docker-entrypoint-new.sh
+++ tool2-image/docker-entrypoint-new.sh
@@ -2,22 +2,28 @@
 
 set -xe
 
+mkdir -p /mnt/shared/jobs
+echo "running" > /mnt/shared/jobs/${JOB_NAME}
+
 S_USER=scipionuser
 S_USER_HOME=/home/${S_USER}
 
 # run base-image's docker-entrypoint-base.sh
 /docker-entrypoint-base.sh
 
-export PATH="/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/VirtualGL/bin:/opt/TurboVNC/bin"
+#export PATH="/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/VirtualGL/bin:/opt/TurboVNC/bin"
 export DISPLAY=scipion-master-svc-x11-${INSTANCE_NAME}:1
 
 # Required steps by the EMAN2
-unset LD_LIBRARY_PATH
-export PATH="/scipion-tool/relion/bin:${S_USER_HOME}/eman2-sphire-sparx/bin:${PATH}"
+#unset LD_LIBRARY_PATH
+
+#export PATH="/scipion-tool/relion/bin:${S_USER_HOME}/eman2-sphire-sparx/bin:${PATH}"
 
 cd "$JOB_WORKDIR"
 
 #${S_USER_HOME}/scipion3/scipion3 run $TOOL_COMMAND
 echo "$TOOL_COMMAND"
-sleep infinity
-#$TOOL_COMMAND
+#sleep infinity
+$TOOL_COMMAND
+
+echo "done" > /mnt/shared/jobs/${JOB_NAME}
