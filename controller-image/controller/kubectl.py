354d5a8f3423207de9b06c6a76c41cd10775af80
Josef Handl <handljosef@gmail.com>
2023-03-10 12:54:20 +0000

    save

--- controller-image/controller/kubectl.py
+++ controller-image/controller/kubectl.py
@@ -10,6 +10,7 @@ class Kubectl:
     def __init__(self, config, namespace, instance_name):
         self.config = config
         self.namespace = namespace
+        self.instance_name = instance_name
 
         self.api_apps = client.AppsV1Api(client.ApiClient(config))
         self.api_batch = client.BatchV1Api(client.ApiClient(config))
@@ -17,18 +18,51 @@ class Kubectl:
         # test namespace
         self.api_apps.list_namespaced_deployment(self.namespace)
 
-    def list_masters(self):
+    def _get_x_name(self, x_name):
+        return f"scipion-{x_name}-{self.instance_name}"
+
+    def _list_deployments(self):
         items = self.api_apps.list_namespaced_deployment(self.namespace).items
         return list(map(lambda deployment: deployment.metadata.name, items))
 
-    def list_tools(self):
+    def _list_jobs(self):
         items = self.api_batch.list_namespaced_job(self.namespace).items
         return list(map(lambda job: job.metadata.name, items))
 
-    def filter_masters(self, instance_name):
-        prefixed_name = f"{k8s_prefix_master}-{instance_name}"
-        return list(filter(lambda master: master.startswith(prefixed_name), self.list_masters()))
+    def filter_masters(self):
+        return list(filter(lambda master: master.startswith(self._get_x_name("master")), self._list_deployments()))
 
-    def filter_tools(self, instance_name):
-        prefixed_name = f"{k8s_prefix_tool}-{instance_name}"
-        return list(filter(lambda tool: tool.startswith(prefixed_name), self.list_tools()))
+    def filter_tools(self):
+        return list(filter(lambda tool: tool.startswith(self._get_x_name("tool-job")), self._list_jobs()))
+
+    def filter_specials(self):
+        return list(filter(lambda tool: tool.startswith(self._get_x_name("firefox")), self._list_jobs()))
+
+    def kill_master(self):
+        masters = self.filter_masters()
+        if not masters or len(masters) != 1:
+            return False
+
+        self.api_apps.delete_namespaced_deployment(masters[0], self.namespace)
+        return True
+
+    def kill_tools(self):
+        tools = self.filter_tools()
+        if not tools:
+            return False
+
+        for t in tools:
+            self.api_batch.delete_namespaced_job(t, self.namespace)
+
+        return True
+
+    def kill_specials(self):
+        specials = self.filter_specials()
+        if not specials:
+            return False
+
+        for s in specials:
+            # specials contains only jobs in this version
+            self.api_batch.delete_namespaced_job(s, self.namespace)
+
+        return True
