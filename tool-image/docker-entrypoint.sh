6c807c5df496bcfc2c7f84e41b7480b51b5fa558
Josef Handl <handljosef@gmail.com>
2023-03-15 14:17:32 +0000

    controller, master, tool: solve xserver authentication instead of disabling ACL

--- tool-image/docker-entrypoint.sh
+++ tool-image/docker-entrypoint.sh
@@ -10,7 +10,19 @@ S_USER_HOME=/home/${S_USER}
 
 export PATH="/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/VirtualGL/bin:/opt/TurboVNC/bin"
 
-export DISPLAY=scipion-master-svc-x11-${INSTANCE_NAME}:1
+export DISPLAY=scipion-vnc-svc-x11-${INSTANCE_NAME}:1
+
+# Wait for the xorg (vnc container) (existency of xauthority means that xserver in the vnc container is running)
+while [ ! -f /mnt/shared/.Xauthority ]; do
+    sleep 2
+done
+# set target to the shared xauthority and extract cookie
+export XAUTHORITY=/mnt/shared/.Xauthority
+x11_cookie="$(echo $(xauth list) | awk '{print $3}')"
+# set target to the correct path of the local xauthority and create new entry with the cookie
+export XAUTHORITY=${S_USER_HOME}/.Xauthority
+xauth add "${DISPLAY}" "MIT-MAGIC-COOKIE-1" "${x11_cookie}"
+
 
 #echo "$TOOL_COMMAND" > ${S_USER_HOME}/ScipionUserData/`date +%s`.txt
 #
@@ -24,6 +36,7 @@ export DISPLAY=scipion-master-svc-x11-${INSTANCE_NAME}:1
 #fi
 #
 #sleep 999999
+
 
 tool_log_dir="/mnt/vol-project/scipion-docker/tool-logs"
 tool_log_file="${tool_log_dir}/${HOSTNAME}.txt"
