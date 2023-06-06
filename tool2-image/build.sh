e59ad55ec4a330ea0a3f81f3bb715eb79dd10f94
Josef Handl <handljosef@gmail.com>
2023-06-06 08:31:09 +0000

    tool2: add cistem bin, uncomment lines in build script

--- tool2-image/build.sh
+++ tool2-image/build.sh
@@ -13,13 +13,13 @@ fi
 docker build --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:base-dev -f Dockerfile.base .
 docker build --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:all-dev -f Dockerfile.all .
 
-#docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:xmipp3-$tag -f Dockerfile.xmipp3 .
-#docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:eman2-$tag -f Dockerfile.eman2 .
-#docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:relion-$tag -f Dockerfile.relion .
-#docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:cistem-$tag -f Dockerfile.cistem .
-#docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:spider-$tag -f Dockerfile.spider .
+docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:xmipp3-$tag -f Dockerfile.xmipp3 .
+docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:eman2-$tag -f Dockerfile.eman2 .
+docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:relion-$tag -f Dockerfile.relion .
+docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:cistem-$tag -f Dockerfile.cistem .
+docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:spider-$tag -f Dockerfile.spider .
 #docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:phenix-$tag -f Dockerfile.phenix .
-#docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:appion-$tag -f Dockerfile.appion .
+docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:appion-$tag -f Dockerfile.appion .
 docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:gautomatch-$tag -f Dockerfile.gautomatch .
 docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:motioncorr-$tag -f Dockerfile.motioncorr .
 docker build --progress plain --build-arg RELEASE_CHANNEL="$tag" -t hub.cerit.io/scipion/scipion-tool2:gctf-$tag -f Dockerfile.gctf .
